13f0eb6002bb0089187570d9b9edbdd83e2ddfb2
Anh Tuấn Cao <72505667+AnhtuanCaoo@users.noreply.github.com>
2021-08-19 16:28:38 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\JuiceController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/juices',[JuiceController::class,'index']);
+Route::post('/juices',[JuiceController::class,'store']);
+Route::get('/fetch-juices',[JuiceController::class,'fetchjuices']);
+Route::get('edit-juice/{id}',[JuiceController::class,'edit']);
+Route::put('update-juice/{id}',[JuiceController::class,'update']);
+Route::delete('delete-juice/{id}',[JuiceController::class,'destroy']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
