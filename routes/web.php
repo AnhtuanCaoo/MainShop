9942867b78abdc7b3ea75399c1a109f7da57592f
Anh Tuấn Cao <72505667+AnhtuanCaoo@users.noreply.github.com>
2021-08-23 10:35:56 +0700

    Revert "Done Upload file"

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,18 @@
 use App\Http\Controllers\JuiceController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/juices',[JuiceController::class,'index']);
 Route::post('/juices',[JuiceController::class,'store']);
 Route::get('/fetch-juices',[JuiceController::class,'fetchjuices']);
 Route::get('edit-juice/{id}',[JuiceController::class,'edit']);
 Route::put('update-juice/{id}',[JuiceController::class,'update']);
-Route::delete('delete-juice/{id}',[JuiceController::class,'destroy'])->middleware('isATC');
+Route::delete('delete-juice/{id}',[JuiceController::class,'destroy']);
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function (){
-    \UniSharp\LaravelFilemanager\Lfm::routes();
-});
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-
