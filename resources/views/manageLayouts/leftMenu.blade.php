2b9735d1dcb772b4dc0bbfbe39c6ef16b0e68034
Anh Tuấn Cao <72505667+AnhtuanCaoo@users.noreply.github.com>
2021-09-18 16:10:50 +0700

    Revert "Update leftMenu.blade.php"
    This reverts commit 441358052337cd0c953989f6cc0a196fbe369091.

--- resources/views/manageLayouts/leftMenu.blade.php
+++ resources/views/manageLayouts/leftMenu.blade.php
@@ -37,11 +37,12 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                   <!-- Add icons to the links using the .nav-icon class
                        with font-awesome or any other icon font library -->
-                       <li class="nav-item">
+                       <li class="nav-item menu-open">
                         <a href="#" class="nav-link">
                           <i class="nav-icon fas fa-th"></i>
                           <p>
                             Quản lý sản phẩm
+                            <i class="right fas fa-angle-left"></i>
                           </p>
                         </a>
                       </li>
@@ -95,17 +96,6 @@
                         </a>
                       </li>
                     </ul>
-                  </li>
-                  <li class="nav-item">
-                    <form method="POST" action="{{ route('logout') }}">
-                      @csrf
-
-                      <x-dropdown-link :href="route('logout')"
-                              onclick="event.preventDefault();
-                                          this.closest('form').submit();">
-                          {{ __('Log Out') }}
-                      </x-dropdown-link>
-                  </form>
                   </li>
                 </ul>
               </nav>
